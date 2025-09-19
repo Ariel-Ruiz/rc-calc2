c2a1d550943b18df03d4595aac23e02c51a0d4b6
Ariel Ruiz <ruizgarcesariel@gmail.com>
2025-09-18 20:50:33 -0500

    config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/rc-calc2/',
   server: {
     allowedHosts: ['.ngrok-free.app', '.ngrok.io', 'localhost', '.github.io']
   }
